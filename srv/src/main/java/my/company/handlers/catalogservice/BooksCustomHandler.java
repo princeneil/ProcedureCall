df728fff5c4aaea9a5370653bc7c6f44d9b09464
Prince Neil <neil.prince@complianterp.com>
2019-07-18 12:23:53 +0000

    Working action

---
+++ srv/src/main/java/my/company/handlers/catalogservice/BooksCustomHandler.java
@@ -0,0 +1,142 @@
+package  my.company.handlers.catalogservice; 
+
+import com.sap.cloud.sdk.service.prov.api.DataSourceHandler;
+import com.sap.cloud.sdk.service.prov.api.EntityData;
+import com.sap.cloud.sdk.service.prov.api.annotations.Action;
+import com.sap.cloud.sdk.service.prov.api.exception.DatasourceException;
+import com.sap.cloud.sdk.service.prov.api.request.OperationRequest;
+import com.sap.cloud.sdk.service.prov.api.response.OperationResponse;
+import com.sap.cloud.sdk.service.prov.api.ExtensionHelper;
+import java.util.Map;
+import java.util.Arrays;
+import java.util.HashMap;
+
+//import com.sap.cloud.sdk.service.prov.api.operations.Query;
+//import com.sap.cloud.sdk.service.prov.api.request.QueryRequest;
+//import com.sap.cloud.sdk.service.prov.api.response.QueryResponse;
+//import com.sap.cloud.sdk.service.prov.api.operations.Read;
+//import com.sap.cloud.sdk.service.prov.api.request.ReadRequest;
+//import com.sap.cloud.sdk.service.prov.api.response.ReadResponse;
+//import com.sap.cloud.sdk.service.prov.api.operations.Update;
+//import com.sap.cloud.sdk.service.prov.api.request.UpdateRequest;
+//import com.sap.cloud.sdk.service.prov.api.response.UpdateResponse;
+//import com.sap.cloud.sdk.service.prov.api.operations.Create;
+//import com.sap.cloud.sdk.service.prov.api.request.CreateRequest;
+//import com.sap.cloud.sdk.service.prov.api.response.CreateResponse;
+//import com.sap.cloud.sdk.service.prov.api.operations.Delete;
+//import com.sap.cloud.sdk.service.prov.api.request.DeleteRequest;
+//import com.sap.cloud.sdk.service.prov.api.response.DeleteResponse;
+//import com.sap.cloud.sdk.service.prov.api.response.ErrorResponse;
+import java.util.List;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/***
+ * Handler class for entity "Books" of service "CatalogService".
+ * This handler registers custom handlers for the entity OData operations.
+ * For more information, see: https://help.sap.com/viewer/65de2977205c403bbc107264b8eccf4b/Cloud/en-US/6fe3070250ea45b88c35cda209e8324b.html
+ */
+public class BooksCustomHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Action(Name="insertBook", serviceName="CatalogService")
+	public OperationResponse updateStock(OperationRequest actionRequest, ExtensionHelper extensionHelper )
+	{
+		
+		Map<String, Object> parameters = actionRequest.getParameters();
+		DataSourceHandler handler = extensionHelper.getHandler();
+		
+		Map<String, Object> keys = new HashMap<String, Object>();
+		keys.put("ID", Integer.parseInt(parameters.get("bookId").toString()));
+
+		try {
+			//fetching the book details for the id and fetching the stock
+//			EntityData entityData = handler.executeRead("Books", keys, actionRequest.getEntityMetadata().getFlattenedElementNames());
+			List <String> properties = Arrays.asList("ID", "title", "stock");
+			EntityData entityData = handler.executeRead("Books", keys, properties);
+			
+			//Apply multiplier to stock
+			Integer stock = Integer.parseInt(entityData.getElementValue("stock").toString());
+			stock = stock * Integer.parseInt(parameters.get("multiplier").toString());
+			
+			//reconstruct the entityData with new stock level
+			entityData = entityData.getBuilder(entityData).removeElement("stock").addElement("stock", stock).buildEntityData("Books");
+			//update the stock
+			handler.executeUpdate(entityData, keys, false);
+			
+			OperationResponse response = OperationResponse.setSuccess().setEntityData(Arrays.asList(entityData)).response();
+			return response;
+			
+		} catch (DatasourceException e) {
+			logger.error("Error accessing the data", e);
+			return null;
+		}
+	}
+
+
+
+//	@Query(entity = "Books", serviceName = "CatalogService")
+//	public QueryResponse queryBooks(QueryRequest req) {
+//      //TODO: add your custom logic...
+//
+//      //List<Object> resultItems = new ArrayList<Object>();
+//      //return QueryResponse.setSuccess().setData(resultItems).response(); //use this API to return items.
+//      ErrorResponse errorResponse = ErrorResponse.getBuilder()
+//        					.setMessage("Unimplemented Query Operation")
+//        					.setStatusCode(500)
+//        					.response();
+//      return QueryResponse.setError(errorResponse);
+//	}
+
+//	@Read(entity = "Books", serviceName = "CatalogService")
+//	public ReadResponse readBooks(ReadRequest req) {
+//      //TODO: add your custom logic...
+//
+//      //Object data = new Object();
+//      //return ReadResponse.setSuccess().setData(data).response(); //use this API to return an item.
+//      ErrorResponse errorResponse = ErrorResponse.getBuilder()
+//        					.setMessage("Unimplemented Read Operation")
+//        					.setStatusCode(500)
+//        					.response();
+//      return ReadResponse.setError(errorResponse);
+//	}
+
+//	@Update(entity = "Books", serviceName = "CatalogService")
+//	public UpdateResponse updateBooks(UpdateRequest req) {
+//      //TODO: add your custom logic...
+//
+//      //return UpdateResponse.setSuccess().response(); //use this API if the item is successfully modified.
+//      ErrorResponse errorResponse = ErrorResponse.getBuilder()
+//        					.setMessage("Unimplemented Update Operation")
+//        					.setStatusCode(500)
+//        					.response();
+//      return UpdateResponse.setError(errorResponse);
+//	}
+
+//	@Create(entity = "Books", serviceName = "CatalogService")
+//	public CreateResponse createBooks(CreateRequest req) {
+//      //TODO: add your custom logic...
+//
+//      //return CreateResponse.setSuccess().response(); //use this API if the item is successfully created.
+//      ErrorResponse errorResponse = ErrorResponse.getBuilder()
+//        					.setMessage("Unimplemented Create Operation")
+//        					.setStatusCode(500)
+//        					.response();
+//      return CreateResponse.setError(errorResponse);
+//	}
+
+//	@Delete(entity = "Books", serviceName = "CatalogService")
+//	public DeleteResponse deleteBooks(DeleteRequest req) {
+//      //TODO: add your custom logic...
+//
+//      //return DeleteResponse.setSuccess().response(); //use this API if the item is successfully deleted.
+//      ErrorResponse errorResponse = ErrorResponse.getBuilder()
+//        					.setMessage("Unimplemented Delete Operation")
+//        					.setStatusCode(500)
+//        					.response();
+//      return DeleteResponse.setError(errorResponse);
+//	}
+
+}
---
+++ ui/webapp/controller/BaseController.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent",
+	"sap/m/library",
+	"sap/m/MessageBox",
+	"sap/m/MessageToast"	
+], function (Controller, UIComponent, mobileLibrary, MessageBox, MessageToast) {
+	"use strict";
+
+	// shortcut for sap.m.URLHelper
+	var URLHelper = mobileLibrary.URLHelper;
+
+	return Controller.extend("Bookshop.ui.controller.BaseController", {
+		/**
+		 * Convenience method for accessing the router.
+		 * @public
+		 * @returns {sap.ui.core.routing.Router} the router for this component
+		 */
+		getRouter : function () {
+			return UIComponent.getRouterFor(this);
+		},
+
+		/**
+		 * Convenience method for getting the view model by name.
+		 * @public
+		 * @param {string} [sName] the model name
+		 * @returns {sap.ui.model.Model} the model instance
+		 */
+		getModel : function (sName) {
+			return this.getView().getModel(sName);
+		},
+
+		/**
+		 * Convenience method for setting the view model.
+		 * @public
+		 * @param {sap.ui.model.Model} oModel the model instance
+		 * @param {string} sName the model name
+		 * @returns {sap.ui.mvc.View} the view instance
+		 */
+		setModel : function (oModel, sName) {
+			return this.getView().setModel(oModel, sName);
+		},
+
+		/**
+		 * Getter for the resource bundle.
+		 * @public
+		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+		 */
+		getResourceBundle : function () {
+			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+		},
+
+		/**
+		 * Event handler when the share by E-Mail button has been clicked
+		 * @public
+		 */
+		onShareEmailPress : function () {
+			//var oViewModel = (this.getModel("objectView") || this.getModel("worklistView"));
+			//URLHelper.triggerEmail(
+			//	null,
+			//	oViewModel.getProperty("/shareSendEmailSubject"),
+			//	oViewModel.getProperty("/shareSendEmailMessage")
+			//);
+			
+			var oModel = this.getOwnerComponent().getModel();
+			var oEntry = {};
+			oEntry.bookId = 1;
+			oEntry.multiplier = 2;
+
+			var mParams = {};
+			mParams.success = function() {
+				sap.m.MessageToast.show("Create successful");
+			};
+			mParams.error = this.onErrorCall;
+			mParams.urlParameters = oEntry;
+			oModel.create("/insertBook", oEntry, mParams);			
+		},
+		
+		onErrorCall: function(oError) {
+			if (oError.statusCode === 500 || oError.statusCode === 400 || oError.statusCode === "500" || oError.statusCode === "400") {
+				var errorRes = JSON.parse(oError.responseText);
+				if (!errorRes.error.innererror) {
+					sap.m.MessageBox.alert(errorRes.error.message.value);
+				} else {
+					if (!errorRes.error.innererror.message) {
+						sap.m.MessageBox.alert(errorRes.error.innererror.toString());
+					} else {
+						sap.m.MessageBox.alert(errorRes.error.innererror.message);
+					}
+				}
+				return;
+			} else {
+				sap.m.MessageBox.alert(oError.response.statusText);
+				return;
+			}
+		}		
+		
+	});
+
+});
